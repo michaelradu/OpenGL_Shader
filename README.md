e8540b595e0e174985c138db6ffaeb34e1316c65
Michael <miihairadu@gmail.com>
2023-06-04 22:33:52 +0300

    Init eyecandy

---
+++ README.md
@@ -0,0 +1,62 @@
+# OpenGL Shader Eyecandy
+
+This project showcases an OpenGL shader program that renders a visual effect using fragment shaders. The shader code is based on a Shadertoy shader I made and has been ported to OpenGL.
+
+## Features
+
+- Visual effect based on a ported Shadertoy shader.
+- Real-time rendering using OpenGL.
+- Full-screen quad rendering.
+- Dynamic time-based animation.
+
+## Dependencies
+
+- OpenGL
+- GLEW
+- GLFW
+
+## Build Instructions
+
+1. Make sure you have the necessary dependencies installed on your system: OpenGL, GLEW, and GLFW.
+
+2. Clone this repository to your local machine.
+
+3. Create a build directory and navigate to it:
+```bash
+mkdir build
+cd build
+```
+
+4. Run CMake to generate the build files:
+```bash
+cmake ..
+```
+
+5. Build the project using the generated build files:
+```bash
+make
+```
+
+6. Run the executable:
+```bash
+./OpenGL_Shader
+```
+
+## Usage
+
+- Once you run the program, a window will appear showing the rendered shader effect.
+- The shader effect will animate and update in real-time based on the current time.
+- Experiment with modifying the shader code in the `main.cpp` file to create your own custom effects :).
+- Explore and tweak the shader parameters to achieve different visual results.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## To do:
+
+- Make the shader full-screen when the user forces the window to be full-screen.
+
+## Contributing
+
+Contributions to this project are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
---
+++ src/main.cpp
@@ -0,0 +1,182 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const char* vertexShaderSource = R"(
+#version 330 core
+
+layout (location = 0) in vec3 vertexPosition;
+
+void main() {
+    gl_Position = vec4(vertexPosition, 1.0);
+}
+)";
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+out vec4 fragColor;
+
+uniform vec2 iResolution;
+uniform float iTime;
+
+vec3 palette(float t) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(0.263, 0.416, 0.557);
+
+    return a + b * cos(6.28318 * (c * t + d));
+}
+
+void main() {
+    vec2 fragCoord = gl_FragCoord.xy;
+    vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
+
+    for (float i = 0.0; i < 4.0; i++) {
+        uv = fract(uv * 1.5) - 0.5;
+
+        float d = length(uv) * exp(-length(uv0));
+
+        vec3 col = palette(length(uv0) + i * 0.4 + iTime * 0.4);
+
+        d = sin(d * 8.0 + iTime) / 8.0;
+        d = abs(d);
+
+        d = pow(0.01 / d, 1.2);
+
+        finalColor += col * d;
+    }
+
+    fragColor = vec4(finalColor, 1.0);
+}
+)";
+
+void error_callback(int error, const char* description) {
+    std::cerr << "Error: " << description << std::endl;
+}
+
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    glfwSetErrorCallback(error_callback);
+
+    // Create a windowed mode GLFW window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Shader", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Compile and link the shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    GLint success;
+    GLchar infoLog[512];
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cerr << "Vertex shader compilation failed:\n" << infoLog << std::endl;
+    }
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cerr << "Fragment shader compilation failed:\n" << infoLog << std::endl;
+    }
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "Shader program linking failed:\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Create and bind vertex buffer object (VBO) and vertex array object (VAO)
+    GLuint VBO, VAO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Define vertex data (a full-screen quad)
+    GLfloat vertices[] = {
+        -1.0f, -1.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,
+        -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f, 0.0f
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glBindVertexArray(0);
+
+    // Render loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Use the shader program
+        glUseProgram(shaderProgram);
+
+        // Set uniform variables
+        GLint resolutionLoc = glGetUniformLocation(shaderProgram, "iResolution");
+        glUniform2f(resolutionLoc, 800, 600);
+
+        GLint timeLoc = glGetUniformLocation(shaderProgram, "iTime");
+        glUniform1f(timeLoc, glfwGetTime());
+
+        // Bind the VAO and draw
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        glBindVertexArray(0);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Clean up
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
+
